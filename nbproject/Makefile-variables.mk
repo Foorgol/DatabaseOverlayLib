62eda98ddeed147c27d426cf67daf79f8d48f637
Volker Knollmann <foorgol@yahoo.com>
2014-01-31 21:39:54 -0500

    Introduced a new, up to now unused compiler configuration "UnitTesting" for the later execution of unit tests

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -22,6 +22,14 @@ CND_ARTIFACT_PATH_Release=dist/Release/CLang-Linux-x86/libDatabaseOverlayLib.so.
 CND_PACKAGE_DIR_Release=dist/Release/CLang-Linux-x86/package
 CND_PACKAGE_NAME_Release=DatabaseOverlayLib.tar
 CND_PACKAGE_PATH_Release=dist/Release/CLang-Linux-x86/package/DatabaseOverlayLib.tar
+# UnitTesting configuration
+CND_PLATFORM_UnitTesting=CLang-Linux-x86
+CND_ARTIFACT_DIR_UnitTesting=dist/UnitTesting/CLang-Linux-x86
+CND_ARTIFACT_NAME_UnitTesting=libDatabaseOverlayLib.so.1.0.0
+CND_ARTIFACT_PATH_UnitTesting=dist/UnitTesting/CLang-Linux-x86/libDatabaseOverlayLib.so.1.0.0
+CND_PACKAGE_DIR_UnitTesting=dist/UnitTesting/CLang-Linux-x86/package
+CND_PACKAGE_NAME_UnitTesting=DatabaseOverlayLib.tar
+CND_PACKAGE_PATH_UnitTesting=dist/UnitTesting/CLang-Linux-x86/package/DatabaseOverlayLib.tar
 #
 # include compiler specific variables
 #
